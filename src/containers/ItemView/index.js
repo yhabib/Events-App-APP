4da691239176e3c229ee426da64b00ef53963b8b
Yusef Habib <habibfernandez@gmail.com>
2017-03-15 11:20:12 +0100

    New component

---
+++ src/containers/ItemView/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import ItemView from './../../ItemView';
+
+class ItemViewContainer extends Component {
+
+  render() {
+    return <ItemView />
+  }
+}
+
+
+export default ItemViewContainer;
