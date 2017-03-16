8fdc54e8f9c634861296655f782fd80fb20ff66e
Eddy Savri <masterneo974@me.com>
2017-03-16 12:16:55 +0100

    create intro page

---
+++ src/components/Intro/intro.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class intro extends Component{
+  render(){
+    retrun(
+      <div className="intro">
+      <img src="intro.jpg" />
+      </div>
+    );
+  }
+}
+
+export default intro;
