64f999f9cd9a96a3f2721facfac108542cd3985e
Yusef Habib <habibfernandez@gmail.com>
2017-03-16 14:21:05 +0100

    Switch Component Powerrrful

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 const backgroundImage = require('./intro.jpg');
 
 const style = {
   height: "100%",
   width: "100%",
-  background: `#fff url('${backgroundImage}') no-repeat center top`,
+  background: `#000 url('${backgroundImage}') no-repeat center top`,
   backgroundSize: 'contain'
 };
 
+
 export default () => (
   <div style={ style }>
+    <Link to="/events/grid"><button>CLICK</button></Link>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import store from './store';
-// import Layout from './components/Layout';
 import Home from './components/Home';
 import HeaderContainer from './containers/Header';
 import ViewContainer from './containers/View';
@@ -21,19 +20,22 @@ import ItemViewContainer from './containers/ItemView';
 
 import './index.css';
 
-injectTapEventPlugin();    // Needed for onTouchTap
+injectTapEventPlugin();
+
 
 const Root = () => (
   <Provider store={ store }>
     <MuiThemeProvider>
       <Router>
         <div style={{width: "100%", height: "100%" }}>
-          {/*<HeaderContainer />*/}
           <Switch>
             <Route exact path="/" component={ Home } />
-            {/*<Route path="/events/grid" component={ ViewContainer } />*/}
-            {/*<Route path="/events/list" component={ ViewContainer } />*/}
-            {/*<Route path="/events/:id" component={ ItemViewContainer }/>*/}
+            <div>
+              <HeaderContainer />
+              <Route path="/events/grid" component={ ViewContainer } />
+              <Route path="/events/list" component={ ViewContainer } />
+              <Route path="/events/:id" component={ ItemViewContainer }/>
+            </div>
           </Switch>
         </div>
       </Router>
