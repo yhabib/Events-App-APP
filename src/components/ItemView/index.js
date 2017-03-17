0f66dcc2e9e726ad7aa9e3f0776d9c33174d8a5d
Yusef Habib <habibfernandez@gmail.com>
2017-03-17 08:59:42 +0100

    Added Maps integration. no KEY

--- src/components/ItemView/index.js
+++ src/components/ItemView/index.js
@@ -1,48 +1,61 @@
 import React, { Component } from 'react';
+
+import { MAPS_URL } from './../../store/constants';
 import './index.css';
 
 class ItemView extends Component {
   render() {
-    return <Item event={ this.props.event } />
+    return <Item event={this.props.event} />
   }
 }
 
 function Item(props) {
   const event = props.event || {};
   const location = props.event.location || {};
-
+  const q = `q=${location.street}+${location.number},${location.city}+${location.country}`;
+  console.log(q);
+  
   return (
     <div className="posterItemContainer">
-        <hr />
-        <img src={ event.eventImage } className="posterItemView" alt={ event.name } />
-        <div className="data">
-            <h3>{event.name}</h3>
-            <h4>{event.eventStartDate}</h4>
-            <p>{event.description}</p>
+      <hr />
+      <img src={event.eventImage} className="posterItemView" alt={event.name} />
+      <div className="data">
+        <h3>{event.name}</h3>
+        <h4>{event.eventStartDate}</h4>
+        <p>{event.description}</p>
 
-            <div>
-            <hr />
-                <div className="boxLeft">
-                <h4>Adresse</h4>
-                    <p>{location.street} {location.number}</p>
-                    <p>{location.postCode} {location.city}</p>
-                    <p>{location.country}</p>
-                </div>
+        <div>
+          <hr />
+          <div className="boxLeft">
+            <h4>Adresse</h4>
+            <p>{location.street} {location.number}</p>
+            <p>{location.postCode} {location.city}</p>
+            <p>{location.country}</p>
+          </div>
 
-                <div className="boxRight">
-                <h4>Contacts</h4>
-                    <p>Tel: 0269240000</p>
-                    <p>Mail: info@info.ch</p>
-                    <p>Web: www.go.ch</p>
-                </div>
-            </div>
-            <hr />
-            <div className="map">
-
-            <h4>Map</h4>
-            <img width="500px" height="300px" alt="Map"/>
-            </div>
+          <div className="boxRight">
+            <h4>Contacts</h4>
+            <p>Tel: 0269240000</p>
+            <p>Mail: info@info.ch</p>
+            <p>Web: www.go.ch</p>
+          </div>
         </div>
+        <hr />
+        <div className="map">
+
+          <h4>Map</h4>
+          <Map iframe='iframe' src={MAPS_URL + q} height="500" width="500" />,
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Map(props) {
+  const Iframe = props.iframe;  
+  return (
+    <div>
+      <Iframe src={props.src} height={props.height} width={props.width} />
     </div>
   )
 }
